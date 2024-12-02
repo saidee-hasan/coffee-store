f0bc6945bcb9f40576d1783b679f94502924d18f
saideeWD <mdsaideehasan@gmail.com>
2024-12-02 19:39:21 +0600

    mongodb connected

---
+++ client/src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UpdateCoffee() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateCoffee
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AddCoffee from './components/AddCoffee.jsx'
+import UpdateCoffee from './components/updateCoffee.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },{
+    path:"/addCoffee",
+    element:<AddCoffee/>
+  },{
+    path:"/updateCoffee",
+    element:<UpdateCoffee/>
+  }
+
+
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
